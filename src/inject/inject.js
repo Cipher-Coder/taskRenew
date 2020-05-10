7fc11801fd911b2933ebcc6f540abdd5a7b389d9
Matt <matthew.rebehn@gmail.com>
2020-05-10 18:41:55 -0400

    Added color to form input | centered min time | worked with inject.js | Added submit button

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,20 @@
-chrome.extension.sendMessage({}, function(response) {
-	let readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+chrome.extension.sendMessage({}, function (response) {
+  let readyStateCheckInterval = setInterval(function () {
+    if (document.readyState === 'complete') {
+      clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
-	}
-	}, 10);
+      // ----------------------------------------------------------
+      // This part of the script triggers when page is done loading
+      console.log('Hello. This message was sent from scripts/inject.js');
+      // ----------------------------------------------------------
+    }
+  }, 10);
 });
+
+/*let myInt = setInterval(setTime, 1000);
+
+function setTime() {
+  let d = new Date();
+  let t = d.toLocaleTimeString();
+  console.log('Current Time: ' + t);
+}*/
