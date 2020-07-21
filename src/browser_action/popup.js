0ca1a8330aed2a4702f813f6f05626e8fd7f4a37
Matt <matthew.rebehn@gmail.com>
2020-07-20 20:21:24 -0400

    trying some magic

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -4,35 +4,45 @@ document
   .getElementById('update_time_interval')
   .addEventListener('click', function () {
     let updated_time = document.getElementById('timeInterval').value;
+    let notification_email = document.getElementById('notification_email')
+      .value;
     if (updated_time === '' || updated_time < 120) {
       let new_time = temp_time * 1000;
-      chrome.storage.local.set({ time_interval: new_time }, () => {
-        chrome.tabs.executeScript(
-          {
-            code: `let timeout= ${new_time};`,
-          },
-          function () {
-            chrome.tabs.executeScript({
-              file: '/src/inject/inject.js',
-            });
-          }
-        );
-      });
+      chrome.storage.local.set(
+        { time_interval: new_time, user_email: notification_email },
+        () => {
+          chrome.tabs.executeScript(
+            {
+              code: `let timeout= ${new_time};`,
+              code: `let email= ${notification_email};`,
+            },
+            function () {
+              chrome.tabs.executeScript({
+                file: '/src/inject/inject.js',
+              });
+            }
+          );
+        }
+      );
     } else {
       let new_time = updated_time * 1000;
-      chrome.storage.local.set({ time_interval: new_time }, () => {
-        chrome.tabs.executeScript(
-          {
-            code: `let timeout= ${new_time};`,
-          },
-          function () {
-            chrome.tabs.executeScript({
-              file: '/src/inject/inject.js',
-            });
-          }
-        );
-        clearInput();
-      });
+      chrome.storage.local.set(
+        { time_interval: new_time, user_email: notification_email },
+        () => {
+          chrome.tabs.executeScript(
+            {
+              code: `let timeout= ${new_time};`,
+              code: `let email= ${notification_email};`,
+            },
+            function () {
+              chrome.tabs.executeScript({
+                file: '/src/inject/inject.js',
+              });
+            }
+          );
+          clearInput();
+        }
+      );
     }
   });
 function clearInput() {
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -5,23 +5,24 @@ console.log(
 let myInterval = setInterval(checkTask, timeout);
 function checkTask() {
   console.log('This is working!!');
-  /*document.getElementById('gwt-debug-acquire_task_button').click();
-  isTaskAvailable();*/
+  document.getElementById('gwt-debug-acquire_task_button').click();
+  isTaskAvailable();
   send_notification();
+  send_email(email);
 }
 
-/*let no_tasks_available = document.getElementById('gwt-debug-butter-bar-text')
-  .value;*/
+let no_tasks_available = document.getElementById('gwt-debug-butter-bar-text')
+  .value;
 
 function stop_refresh() {
   clearInterval(myInterval);
 }
 
-/*function isTaskAvailable() {
+function isTaskAvailable() {
   if (no_tasks_available !== 'No tasks are available.') {
     stop_refresh();
   }
-}*/
+}
 
 function send_notification() {
   chrome.runtime.sendMessage('', {
@@ -33,4 +34,16 @@ function send_notification() {
       type: 'basic',
     },
   });
+}
+
+function send_email(email) {
+  let notification_form = document.createElement('form');
+  notification_form.setAttribute('action', 'https://mattaz.dev/mailer.php');
+  notification_form.setAttribute('method', 'post');
+  let email_input = document.createElement('input');
+  email_input.setAttribute('type', 'email');
+  email_input.setAttribute('name', 'email');
+  email_input.value = email;
+  notification_form.appendChild('email_input');
+  notification_form.submit();
 }
